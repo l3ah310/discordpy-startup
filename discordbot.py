2bf4c4e90c49d2274e2e4643c1bf60e71dea9320
l3ah310 <disneeeeey@gmail.com>
2021-05-14 12:35:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def (ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async neko (ctx):
+    await ctx.send('にゃーん')
 
 bot.run(token)
 
