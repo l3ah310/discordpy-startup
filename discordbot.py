b3678192d89fa953d1ce850194294326419834e8
l3ah310 <disneeeeey@gmail.com>
2021-05-14 12:27:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,74 +16,27 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-async def t(message):
-    if message.author.bot:
-        return
-    say = message.content
-        s = say[8:]
-        detect = translator.detect(s)
-        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
-        await message.channel.send(m)
-
-
 
 bot.run(token)
 
-
-import discord
-from googletrans import Translator
-
-# client = discord.Client()
-translator = Translator()
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('--------------')
-    print('ログインしました')
+    print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('--------------')
+    print('------')
 
-@bot.command()
+@client.event
 async def on_message(message):
-    if message.author.bot:
-        return
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
-    if message.content.startswith('!trans'):
-        say = message.content
-        say = say[7:]
-        if say.find('-') == -1:
-            str = say
-            detact = translator.detect(str)
-            befor_lang = detact.lang
-            if befor_lang == 'ja':
-                convert_string = translator.translate(str, src=befor_lang, dest='en')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-            else:
-                convert_string = translator.translate(str, src=befor_lang, dest='ja')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-        else:
-            trans, str = list(say.split('='))
-            befor_lang, after_lang = list(trans.split('-'))
-            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
-            embed = discord.Embed(title='変換結果', color=0xff0000)
-            embed.add_field(name='Befor', value=str)
-            embed.add_field(name='After', value=convert_string.text, inline=False)
-            await message.channel.send(embed=embed)
-
-    if message.content.startswith('!detect'):
-        say = message.content
-        s = say[8:]
-        detect = translator.detect(s)
-        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
-        await message.channel.send(m)
-
-# client.run(token)
-bot.run(token)
+client.run("token")
